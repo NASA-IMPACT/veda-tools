3e8c4bb7f0acc4e8bdfcbc84aa9e946d3284129d
Ram Sharan Rimal <rsr0012@uah.edu>
2025-07-30 12:35:08 -0500

    test release it bug

--- .release-it.js
+++ .release-it.js
@@ -18,7 +18,7 @@ module.exports = {
     // changelog: 'git log --pretty=format:%s ${latestTag}...HEAD' // this is overridden by the @release-it/conventional-changelog's changelog
   },
   github: {
-    release: debug ? false : true,
+    release: false ,
     releaseName: "v${version}",
     autoGenerate: false,
     releaseNotes: getReleaseNotes,
