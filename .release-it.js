f406e67181ab9e6db4fe254fb908ed12743e5cd5
Ram Sharan Rimal <rsr0012@uah.edu>
2025-07-30 14:32:03 -0500

    make release it run even if debug undefined

--- .release-it.js
+++ .release-it.js
@@ -6,25 +6,25 @@ module.exports = {
     'after:release': 'echo "VERSION_NUMBER=v${version}" >> "$GITHUB_OUTPUT"'
   },
   git: {
-    release: (!debug)? false : true,
+    release: (!!debug)? false : true,
     requireBranch: ["main", "develop"],
     commitMessage: "chore: release v${version}",
     tagName: 'v${version}',
     tagAnnotation: 'Release v${version}',
     pushArgs: ['--follow-tags'],
-    requireCleanWorkingDir: (!debug)? false : true,
-    requireUpstream: (!debug) ? false : true,
+    requireCleanWorkingDir: (!!debug)? false : true,
+    requireUpstream: (!!debug) ? false : true,
     getLatestTagFromAllRefs: true
     // changelog: 'git log --pretty=format:%s ${latestTag}...HEAD' // this is overridden by the @release-it/conventional-changelog's changelog
   },
   github: {
-    release: (!debug) ? false : true ,
+    release: (!!debug) ? false : true ,
     releaseName: "v${version}",
     autoGenerate: false,
     releaseNotes: getReleaseNotes,
   },
   npm: {
-    publish: (!debug) ? false : true
+    publish: (!!debug) ? false : true
   },
   publishConfig: {
     registry: "https://registry.npmjs.org"
